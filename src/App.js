c87ea7a07ff98c99367c86932444f0e966f011c7
garrettvercoe <garrettvercoe@gmail.com>
2019-06-19 15:40:17 -0400

    Installed Konva + Tone JS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import { Stage, Layer, Rect, Text } from "react-konva";
+import Konva from "konva";
+import Tone from "tone";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ColoredRect extends React.Component {
+  state = {
+    color: "green"
+  };
+  handleClick = () => {
+    this.setState({
+      color: Konva.Util.getRandomColor()
+    });
+  };
+  render() {
+    return (
+      <Rect
+        x={20}
+        y={20}
+        width={900}
+        height={900}
+        fill={this.state.color}
+        shadowBlur={5}
+        onClick={this.handleClick}
+      />
+    );
+  }
 }
 
-export default App;
+export default class App extends Component {
+  render() {
+    // Stage is a div container
+    // Layer is actual canvas element (so you may have several canvases in the stage)
+    // And then we have canvas shapes inside the Layer
+    return (
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          <Text text="Try click on rect" />
+          <ColoredRect />
+        </Layer>
+      </Stage>
+    );
+  }
+}
