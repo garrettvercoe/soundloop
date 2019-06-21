266935585e89f13ea631cbbc0b34632ab79589e6
garrettvercoe <garrettvercoe@gmail.com>
2019-06-21 12:53:41 -0400

    toneButton

--- src/components/ToneLibrary.js
+++ src/components/ToneLibrary.js
@@ -1,5 +1,15 @@
 import React from "react";
 
+class ToneButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { color: "#000" };
+  }
+  render() {
+    return <div>test</div>;
+  }
+}
+
 export default class LibraryContainer extends React.Component {
   render() {
     return <div> This is the container</div>;
