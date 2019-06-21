a3ca2118c69be687e8922306b4a1b577c271aac1
garrettvercoe <garrettvercoe@gmail.com>
2019-06-21 12:26:08 -0400

    starting build

--- src/App.js
+++ src/App.js
@@ -4,42 +4,20 @@ import { Stage, Layer, Rect, Text, Circle } from "react-konva";
 import Konva from "konva";
 import Tone from "tone";
 
-class ColoredCirc extends React.Component {
-  state = {
-    color: "green"
-  };
-  handleClick = () => {
-    this.setState({
-      color: Konva.Util.getRandomColor()
-    });
-  };
-  render() {
-    return (
-      <Circle
-        x={300}
-        y={300}
-        width={500}
-        height={500}
-        fill={this.state.color}
-        shadowBlur={5}
-        onClick={this.handleClick}
-      />
-    );
-  }
-}
-
 export default class App extends Component {
   render() {
     // Stage is a div container
     // Layer is actual canvas element (so you may have several canvases in the stage)
     // And then we have canvas shapes inside the Layer
     return (
-      <Stage width={window.innerWidth} height={window.innerHeight}>
-        <Layer>
-          <Text text="Try clicking on the circle!" />
-          <ColoredCirc />
-        </Layer>
-      </Stage>
+      <React.Fragment>
+        <div>container placeholder</div>
+        <Stage width={window.innerWidth} height={window.innerHeight}>
+          <Layer>
+            <Text text="Konva Stage" />
+          </Layer>
+        </Stage>
+      </React.Fragment>
     );
   }
 }
