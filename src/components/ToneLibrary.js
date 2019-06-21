59e29b774350c4a2955d226902a1c728830103bc
garrettvercoe <garrettvercoe@gmail.com>
2019-06-21 14:26:57 -0400

    made ToneLibrary.js & Buttons

--- src/components/ToneLibrary.js
+++ src/components/ToneLibrary.js
@@ -1,17 +1,79 @@
 import React from "react";
 
+const LibListStyle = {
+  textAlign: "left",
+  margin: 0,
+  padding: 0,
+  position: "relative"
+};
+
+const LibListItemStyle = {
+  display: "inline-block",
+  verticalAlign: "top",
+  padding: ".25rem"
+};
+
+const card = {
+  position: "absolute",
+  top: "30%",
+  left: "20%",
+  width: "15rem",
+  height: "14rem",
+  backgroundColor: "#fff",
+  borderRadius: "1%"
+};
+
+const contentContainer = { padding: "2rem" };
+
 class ToneButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { color: "#000" };
-  }
   render() {
-    return <div>test</div>;
+    return (
+      <button
+        style={{
+          borderRadius: "100%",
+          cursor: "pointer",
+          backgroundColor: this.props.color,
+          width: "2rem",
+          height: "2rem",
+          border: "none"
+        }}
+      />
+    );
+  }
+}
+
+class Library extends React.Component {
+  render() {
+    return (
+      <ul style={LibListStyle}>
+        {this.props.colors.map(color => (
+          <li style={LibListItemStyle} key={color}>
+            <ToneButton color={color} />
+          </li>
+        ))}
+      </ul>
+    );
   }
 }
 
 export default class LibraryContainer extends React.Component {
   render() {
-    return <div> This is the container</div>;
+    return (
+      <div style={card}>
+        <div style={contentContainer}>
+          <h3> TONES</h3>
+          <Library
+            colors={[
+              "#fe6b12",
+              "#fec52b",
+              "#029e4e",
+              "#e11a79",
+              "#6581c9",
+              "#7f47dd"
+            ]}
+          />
+        </div>
+      </div>
+    );
   }
 }
