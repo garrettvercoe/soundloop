eac644328e1d51ccbea4ba1d26d641d0536b4e63
Eric Duong <ericduong@Erics-MacBook-Pro-4.local>
2019-06-21 12:51:11 -0400

    Add window dimensions

--- src/App.js
+++ src/App.js
@@ -5,6 +5,22 @@ import Konva from "konva";
 import Tone from "tone";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { width: 0, height: 0 };
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+  updateWindowDimensions() {
+    this.setState({ width: window.innerWidth, height: window.innerHeight });
+    console.log(this.state);
+  }
   render() {
     // Stage is a div container
     // Layer is actual canvas element (so you may have several canvases in the stage)
