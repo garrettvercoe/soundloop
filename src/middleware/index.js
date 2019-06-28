3745dc6473de8a02137d37ccce31be11a237e367
Eric Duong <ericduong@Erics-MacBook-Pro-4.local>
2019-06-28 13:20:15 -0400

    Create logger

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,5 +1,5 @@
-import checker from "./checker";
 import logger from "./logger";
 import thunk from "redux-thunk";
 import { applyMiddleware } from "redux";
-export default applyMiddleware(thunk, checker, logger);
+
+export default applyMiddleware(thunk, logger);
